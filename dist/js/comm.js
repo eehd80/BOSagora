6a67fd1a90e9724c22b585847f16a0d41086d014
eehd80 <eehd80yul@gmail.com>
2022-10-04 16:46:49 +0900

    community news list

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,96 +1,97 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // header
+    function headerfixed() {
+        $(window).on("scroll", function () {
+            var height = $("#header").height();
+            var scroll = $(window).scrollTop();
+            if (scroll > height) {
+                $("body").addClass("scrolled");
+            } else {
+                $("body").removeClass("scrolled");
+            }
+        });
+    }
+    // function gnb() {
+    //     var parent = $("#gnb");
+    //     (body = $("body")),
+    //         (dep1 = parent.find(">ul>li>a")),
+    //         (sub = parent.find(">ul>li>.sub")),
+    //         (last = parent.find(">ul>li:last .sub ul li:last>a"));
+    //     dep1.on("mouseover focusin", function () {
+    //         var t = $(this);
+    //         if (t.next().children().children().length) {
+    //             body.addClass("on-gnb");
+    //             dep1.removeClass("on");
+    //             // t.addClass("on");
+    //             sub.stop().hide();
+    //             t.next().stop().fadeIn(350);
+    //         } else {
+    //             gnbHide();
+    //         }
+    //     });
+    //     last.on("focusout", function () {
+    //         gnbHide();
+    //     });
+    //     parent.on("mouseleave", function () {
+    //         gnbHide();
+    //     });
+    //     function gnbHide() {
+    //         body.removeClass("on-gnb");
+    //         dep1.removeClass("on");
+    //         sub.stop().hide();
+    //     }
+    // }
 
-  // swiper-intro
-  var swiper = new Swiper(".slide-intro", {
-    // spaceBetween: 30,
-    effect: "fade",
-    centeredSlides: true,
-    loop: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+    function gnb() {
+        let gnb = $("#gnb");
+        header = $("#header");
 
-  // slide-banner1
-  var swiper = new Swiper(".slide-bnr1", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 20,
-      },
-    },
-  });
+        gnb.on("mouseover focusin", function () {
+            header.addClass("active");
+        }).on("mouseleave focusout", function () {
+            header.removeClass("active");
+        });
+    }
 
-  // slide-bnr2
-  var swiper = new Swiper(".slide-bnr2", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: false,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    navigation: {
-      nextEl: ".swiper-button-next-out",
-      prevEl: ".swiper-button-prev-out",
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-        // centeredSlides: false,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        // centeredSlides: false,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 20,        
+    function depth2Bg() {
+        let dep2Height = $(".gnb-1dep .on .gnb-2dep").height(),
+            dep2Bg = $(".gnb-2dep-bg"),
+            dep1Li = $(".gnb-1dep li");
+
+        console.log(dep2Height);
+
+        dep1Li.on("mouseover focusin", function () {
+            dep2Bg = dep2Height;
+            console.log(dep2Height);
+        });
+    }
+
+    headerfixed();
+    gnb();
+    depth2Bg();
+
+    // scrollup
+    $(function () {
+        $.scrollUp();
+    });
+
+    // swiper-intro
+    var swiper = new Swiper(".slide-intro", {
+        // spaceBetween: 30,
+        effect: "fade",
         centeredSlides: true,
-      },
-    },
-  });
-
-
-
-
-
-
-
-
-
-    
-
+        loop: true,
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 });
